cb5262e65065f6b0c2d3ad359d282317848b7671
Tim Wynants <tim.wynants@tomra.com>
2021-03-30 08:51:28 +0200

    [impl] Finished Optional

--- src/Course/Optional.hs
+++ src/Course/Optional.hs
@@ -27,8 +27,8 @@ mapOptional ::
   (a -> b)
   -> Optional a
   -> Optional b
-mapOptional =
-  error "todo: Course.Optional#mapOptional"
+mapOptional f (Full a) = Full $ f a
+mapOptional _ Empty = Empty
 
 -- | Bind the given function on the possible value.
 --
@@ -44,9 +44,8 @@ bindOptional ::
   (a -> Optional b)
   -> Optional a
   -> Optional b
-bindOptional =
-  error "todo: Course.Optional#bindOptional"
-
+bindOptional f (Full a) = f a
+bindOptional _ Empty = Empty
 -- | Return the possible value if it exists; otherwise, the second argument.
 --
 -- >>> Full 8 ?? 99
@@ -58,8 +57,9 @@ bindOptional =
   Optional a
   -> a
   -> a
-(??) =
-  error "todo: Course.Optional#(??)"
+(??) Empty b = b
+(??) (Full x) _ = x
+
 
 -- | Try the first optional for a value. If it has a value, use it; otherwise,
 -- use the second value.
@@ -79,8 +79,9 @@ bindOptional =
   Optional a
   -> Optional a
   -> Optional a
-(<+>) =
-  error "todo: Course.Optional#(<+>)"
+(<+>) Empty b = b
+(<+>) a _ = a
+  
 
 -- | Replaces the Full and Empty constructors in an optional.
 --
@@ -94,8 +95,8 @@ optional ::
   -> b
   -> Optional a
   -> b
-optional =
-  error "todo: Course.Optional#optional"
+optional f default_ a =  mapOptional f a ?? default_
+
 
 applyOptional :: Optional (a -> b) -> Optional a -> Optional b
 applyOptional f a = bindOptional (\f' -> mapOptional f' a) f
